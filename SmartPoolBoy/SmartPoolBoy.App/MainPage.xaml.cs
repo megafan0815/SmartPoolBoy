78cf0ba5c2ae9abab6a02238a5028b67a9b6ba87
CHRISTIAN ERTL <christian.ertl@freenet.de>
2020-07-21 19:33:15 +0200

    first Checkin. Erste Schritte für das Scannen von Modulen. Leider gibt es noch ein Problem, sobald ich auf ARM umstelle. Muss mich da noch etwas weiterbilden ;-).

---
+++ SmartPoolBoy/SmartPoolBoy.App/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.I2c;
+using Windows.Devices.Enumeration;
+using SmartPoolBoy.Konfiguration;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SmartPoolBoy.App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btModuleScannen_Click(object sender, RoutedEventArgs e)
+        {
+            tbScannedDevices.Text = "";
+            var moduleScanner = new ModuleScanner();
+            foreach (var di in moduleScanner.ScanForI2CDevices())
+            {
+                tbScannedDevices.Text += $"\nDevice-Addresse='{di.DeviceSlaveAddress}', Device-Description='{di.DeviceDescription}'";
+            }
+        }
+    }
+}
---
+++ SmartPoolBoy/SmartPoolBoy.Konfiguration/ModuleScanner.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace SmartPoolBoy.Konfiguration
+{
+	public class DeviceInfo
+    {
+		public byte DeviceSlaveAddress { get; }
+		public string DeviceDescription { get; }
+
+        public DeviceInfo(byte deviceSlaveAddress, string deviceDescription)
+        {
+            DeviceSlaveAddress = deviceSlaveAddress;
+            DeviceDescription = deviceDescription;
+        }
+    }
+
+    public class ModuleScanner
+    {
+        public ModuleScanner()
+        {
+
+        }
+
+        public List<DeviceInfo> ScanForI2CDevices()
+        {
+			var result = new List<DeviceInfo>();
+            foreach (var devInfo in FindDevicesAsync().Result)
+            {
+				result.Add(new DeviceInfo(devInfo.Key, devInfo.Value));
+            }
+			return result;
+        }
+
+		public async Task<IEnumerable<KeyValuePair<byte, string>>> FindDevicesAsync()
+		{
+			IList<KeyValuePair<byte, string>> returnValue = new List<KeyValuePair<byte, string>>();
+			// *** 
+			// *** Get a selector string that will return all I2C controllers on the system 
+			// *** 
+			string aqs = I2cDevice.GetDeviceSelector();
+            // *** 
+            // *** Find the I2C bus controller device with our selector string 
+            // *** 
+            DeviceInformationCollection dis = await DeviceInformation.FindAllAsync(aqs).AsTask();
+			if (dis.Count > 0)
+			{
+				const int minimumAddress = 0x08;
+				const int maximumAddress = 0x77;
+				for (byte address = minimumAddress; address <= maximumAddress; address++)
+				{
+					var settings = new I2cConnectionSettings(address);
+					settings.BusSpeed = I2cBusSpeed.FastMode;
+					settings.SharingMode = I2cSharingMode.Shared;
+					// *** 
+					// *** Create an I2cDevice with our selected bus controller and I2C settings 
+					// *** 
+					using (I2cDevice device = await I2cDevice.FromIdAsync(dis[0].Id, settings))
+					{
+						if (device != null)
+						{
+							try
+							{
+								byte[] writeBuffer = new byte[1] { 0 };
+								device.Write(writeBuffer);
+								// *** 
+								// *** If no exception is thrown, there is 
+								// *** a device at this address. 
+								// *** 
+								// TODO hier könnte man jetzt eine Art Device-Info vom Device abfragen
+								//byte[] readBuffer = new byte[1];
+								//device.Read(readBuffer);
+								//if (readBuffer[0] == 0x2D)
+								//{
+								//	returnValue.Add(address);
+								//}
+								returnValue.Add(new KeyValuePair<byte, string>(address, "TODO"));
+							}
+							catch
+							{
+								// *** 
+								// *** If the address is invalid, an exception will be thrown. 
+								// *** 
+							}
+						}
+					}
+				}
+			}
+			return returnValue;
+		}
+	}
+}
